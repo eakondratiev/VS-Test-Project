e19d5ed7dba2a8591e99ed01ffebc91f976203d3
E. Kondratiev <ekondr@hotmail.com>
2016-03-18 00:19:34 +0300

    First Edition

---
+++ HelloWorldProject/HelloWorldProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProject
+{
+  class Program
+  {
+    /// <summary>
+    /// Main entry.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+      Console.WriteLine ("Hello, World of GitHub!");
+    }
+  }
+}
